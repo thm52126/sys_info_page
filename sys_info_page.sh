358a2580843305229eef33985789f4864362a751
Thomas Murphy <tmurphy1@login4.ufhpc>
2020-09-13 17:06:26 -0400

    add some data

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -2,12 +2,18 @@
 # Program to output a system information page
 
 TITLE="Systems Information Report for $HOSTNAME"
+CURRENT_TIME="$(date +"%x %r %Z")"
+TIMESTAMP="Generated $CURRENT_TIME, by $USER"
 
-echo "<html>
+cat << _EOF_
+<html>
     <head>
       <title>$TITLE</title>
     </head>
     <body>
       <hl>$TITLE</h1>
+      <p>$TIMESTAMP</p>
     </body>
-</html>"
+
+</html>
+_EOF_
